f23b2589ddad9705c7acdc49b8570cdfb49aa402
waltereidi <walter-eidi@hotmail.com>
2023-08-28 18:16:37 -0300

    first commit

---
+++ src/components/Utils.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import Paginacao from "@/components/Utils/Paginacao.vue";
+
+export default { 
+    data() {
+        return {
+            paginaAtual: 1, 
+            multiplicador: 1,
+        }
+    },
+    components: {
+        Paginacao
+    },
+    methods: {
+        childRetornaPagiancao(paginacaoRetorno , multiplicador) {
+            this.paginaAtual = paginacaoRetorno;
+            this.multiplicador = multiplicador;
+        }
+    }
+}
+</script>
+<template>
+    <h1>Utils</h1>
+    <Paginacao :quantidade="50" 
+    :multiplicador="5" 
+    @retornaPaginacao="childRetornaPagiancao"
+    :limitePaginacao="6" ></Paginacao>
+
+
+    {{ paginaAtual  }}{{ multiplicador }}
+</template>
