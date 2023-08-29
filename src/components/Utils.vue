753ef3f83c3c27ec1cd0b8928f212ab1245c00cf
waltereidi <walter-eidi@hotmail.com>
2023-08-29 16:13:31 -0300

    inicado componente de modal

--- src/components/Utils.vue
+++ src/components/Utils.vue
@@ -1,15 +1,20 @@
 <script lang="ts">
 import Paginacao from "@/components/Utils/Paginacao.vue";
+import Config from "@/assets/json/bibliotecaconfig.json";
+import ModalImagem from "@/components/Utils/ModalImagem.vue";
 
 export default { 
     data() {
         return {
             paginaAtual: 1, 
             multiplicador: 1,
+            configDataSource: Config,
+            
         }
     },
     components: {
-        Paginacao
+        Paginacao,
+        ModalImagem
     },
     methods: {
         childRetornaPagiancao(paginacaoRetorno , multiplicador) {
@@ -21,13 +26,19 @@ export default {
 </script>
 <template>
     <h1>Utils</h1>
-    <Paginacao :quantidade="50" 
-    :multiplicador="5" 
-    @retornaPaginacao="childRetornaPagiancao"
-    :limitePaginacao="6" 
-    :travarPaginacao="false"
-    ></Paginacao>
+    <Paginacao 
+        :quantidade="50" 
+        :multiplicador="5" 
+        @retornaPaginacao="childRetornaPagiancao"
+        :limitePaginacao="6" 
+        :travarPaginacao="false">
+    </Paginacao>
 
 
-    {{ paginaAtual  }}{{ multiplicador }}
+    <ModalImagem 
+        :srcImagem="configDataSource.capaLivroDefault" 
+        :heightModal="'50'"
+        :widthModal="'50'"
+        :medida="'px'"
+        ></ModalImagem>
 </template>
---
+++ src/components/Utils/ModalImagem.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+export default { 
+    props: {
+            srcImagem: String, 
+            heightModal: String,
+            widthModal: String, 
+            medidaModal: String,
+    },
+    methods: {
+        abrirModal() {
+            
+        }
+    }
+    }
+</script>
+<template>
+ <img :src="srcImagem" :style="{ width: `${widthModal}`}" @click="abrirModal">
+
+
+</template>
+<style scoped>
+
+</style>
